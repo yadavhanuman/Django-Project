60cc817d5c3173a88cfa013bfea59c24d8ca61ae
Hanuman Yadav <hanuman2241@gmail.com>
2024-03-28 11:43:29 +0530

    'Django_project'

---
+++ members/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Members (models.Model):
+    firstname=models.CharField(max_length=255)
+    lastname=models.CharField(max_length=255)
+    
+
+
+
+
+
+
+
+
+
+
+
+# Create your models here.
